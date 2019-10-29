c77d7f39009a141569238c65a96f29a7cae52e8a
Sarah Roberts <sarahr@cyverse.org>
2019-10-29 11:18:53 -0700

    Example query parameter processing

--- docs/main.go
+++ docs/main.go
@@ -21,10 +21,18 @@ import "github.com/cyverse-de/de-stats/api"
 // Returns general information about the API.
 // responses:
 //   200: rootResponse
+//   400: errorResponse
 
 // General information about the API.
 // swagger:response rootResponse
 type rootResponseWrapper struct {
 	// in:body
 	Body api.RootResponse
+}
+
+// Basic error response.
+// swagger:response errorResponse
+type errorResponseWrapper struct {
+	// in:body
+	Body api.ErrorResponse
 }
