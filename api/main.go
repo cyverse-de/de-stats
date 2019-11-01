e996e7ccac423c4184f90f73a95a630630018edc
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-11-01 13:43:14 -0700

    move apps handler to new file

--- api/main.go
+++ api/main.go
@@ -1,9 +1,8 @@
 package api
 
 import (
-	"github.com/cyverse-de/de-stats/cron"
-	"net/http"
 	"github.com/labstack/echo"
+	"net/http"
 )
 
 // RootResponse describes the response of the root endpoint.
@@ -12,10 +11,7 @@ type RootResponse struct {
 	Bar string `json:"bar"`
 }
 
-type AppsResponse struct {
-	Count	int `json:"count"`
-	Apps	[]cron.App	`json:"apps"`
-}
+
 // RootHandler handles get requests to the / endpoint.
 func RootHandler(ctx echo.Context) error {
 	resp := RootResponse{
@@ -25,15 +21,4 @@ func RootHandler(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, resp)
 }
 
-func AppsHandler(ctx echo.Context) error{
-	db := cron.InitDB()
-	amount := 10
-	days := 100
-	apps := cron.GetTopApps(db, amount, days)
-	resp := AppsResponse{
-		Count: amount,
-		Apps:  apps,
-	}
-	return ctx.JSON(http.StatusOK, resp)
 
-}
