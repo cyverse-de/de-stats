dfd8a1a6bd3f9806247c9fcbf53e6ab8127db4c5
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-11-07 12:42:31 -0700

    add response handler to get top users in given time

---
+++ api/usersendpoint.go
@@ -0,0 +1,63 @@
+package api
+
+import (
+	"fmt"
+	"github.com/cyverse-de/de-stats/cron"
+	"github.com/labstack/echo"
+	"net/http"
+	"github.com/cyverse-de/de-stats/util"
+)
+
+type UsersParams struct {
+	//The number of days to include in the response
+	//
+	//in: query
+	//required: false
+	//minimum: 0
+	//maximum: 365
+	//default: 7
+	Days int
+
+	//The number of users to include in the response
+	//
+	//in: query
+	//required: false
+	//minimum: 1
+	//maximum: 1000
+	//default: 10
+	Count int
+}
+
+type UsersResponse struct {
+	Count	int `json:"count"`
+	Users	[]cron.User	`json:"users"`
+}
+
+func UsersHandler(ctx echo.Context) error{
+
+	days, err := util.IntQueryParam(ctx, "days", 1, 0, 365)
+	fmt.Println(days)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, ErrorResponse{Description: err.Error()})
+	}
+	db := cron.InitDB()
+
+	amount, err := util.IntQueryParam(ctx, "count", 10, 1, 1000)
+	fmt.Println(amount)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, ErrorResponse{Description: err.Error()})
+	}
+
+	users, err := cron.getTopUsers(db, amount, days)
+
+	if err != nil{
+		return err
+	}
+
+	resp := AppsResponse{
+		Count: amount,
+		Users:  users,
+	}
+	return ctx.JSON(http.StatusOK, resp)
+
+}
