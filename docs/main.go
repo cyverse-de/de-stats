765a712fe84c01dfe83483ae7679f695d78e212e
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-12-05 10:51:37 -0700

    add docs for users and jobs/submitted

--- docs/main.go
+++ docs/main.go
@@ -37,14 +37,41 @@ type errorResponseWrapper struct {
 	Body api.ErrorResponse
 }
 
-// swagger:route GET /apps getTopApps
+// swagger:route GET /apps GetTopApps
 // Returns the top apps in the given time period.
 // Responses:
 // 200: appsResponse
+// 400: errorResponse
 
 //Top apps in time period.
 //swagger:response appsResponse
 type appsResponseWrapper struct {
 	// in:body
 	Body api.AppsResponse
+}
+
+// swagger:route GET /users GetTopUsers
+// Returns the top users in the given time period.
+// Responses:
+// 200: usersResponse
+// 400: errorResponse
+
+//Top users in time period
+//swagger:response usersResponse
+type usersResponseWrapper struct {
+	// in:body
+	Body api.UsersResponse
+}
+
+// swagger:route GET /jobs/submitted GetSubmittedJobCounts
+// Returns the number of submitted jobs in the given time period for each job type.
+// Responses:
+// 200: usersResponse
+// 400: errorResponse
+
+//Top users in time period
+//swagger:response usersResponse
+type jobsResponseWrapper struct {
+	// in:body
+	Body api.JobsResponse
 }
